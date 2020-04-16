e955ed7f5f4042602ea732b3d93be3e21111167e
Daniel Miranda <daniel_miranda97@hotmail.com>
2020-04-16 20:16:38 -0300

    First Commit

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -48,10 +48,92 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white,fontSize: 18),
               textAlign: TextAlign.center,
+              onSubmitted: (text){
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGif(),
+              builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200,
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) {
+                      return Container();
+                    }else {
+                      return _createGifTable(context, snapshot);
+                    }
+                }
+                return null;
+              },
             ),
           )
         ],
       ),
     );
+  }
+
+  int _getCount(List data){
+    if(_search == null) {
+      return data.length;
+    }else {
+      return data.length + 1;
+    }
+  }
+  
+  
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+      return GridView.builder(
+          padding: EdgeInsets.all(10),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: _getCount(snapshot.data["data"]),
+          itemBuilder: (context, index){
+            if(_search == null || index < snapshot.data["data"].length) {
+              return GestureDetector(
+                child: Image.network(
+                  snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
+              );
+            }else{
+              return Container(
+                child: GestureDetector(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.add, color: Colors.white, size: 70,),
+                      Text("Carregar mais.", style: TextStyle(color: Colors.white, fontSize: 22),)
+                    ],
+                  ),
+                  onTap: (){
+                    setState(() {
+                      _offset += 20;
+                    });
+                  },
+                ),
+              );
+            }
+
+          });
   }
 }
